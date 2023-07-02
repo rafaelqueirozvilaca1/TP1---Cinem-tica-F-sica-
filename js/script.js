6ef0934f48e77c644b750000dab673a7728a4e60
Rafael Queiroz Vilaça <112505616+rafaelqueirozvilaca1@users.noreply.github.com>
2023-07-01 21:27:45 -0300

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-/* marca o link atual do menu */
-let a = document.querySelectorAll ('#menu a');
-let linkAtualDoMenu = window.location.href;
-a.forEach (aEl => {
-    if (aEl == linkAtualDoMenu)
-        aEl.classList.add ('marcar-pagina-atual');
-});
